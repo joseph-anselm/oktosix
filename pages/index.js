390e64065cc63038f980d8d342a03d80a9ca4ced
Joseph Anselm <iamjoeanselm@gmail.com>
2022-04-04 18:53:58 +0100

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
+import { SocialIcon } from "react-social-icons";
 
 export default function index() {
   return (
@@ -15,7 +16,21 @@ export default function index() {
           <div className="ml-5 ">
             <h1 className=" mb-5 text-7xl">Oktosix Motions Pictures</h1>
             <p>Unlimited Entertainment, Movies & More!</p>
-            <button>Watch Now!</button>
+            <button className=" mt-5 flex rounded-full bg-yellow-600 py-2 px-4 font-bold text-white hover:bg-red-700">
+              <svg
+                class="h-6 w-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>{" "}
+              Watch Now!
+            </button>
           </div>
         </div>
       </div>
