5468a4efdc71f18e9dc09b2cb03850a4e518f105
Joseph Anselm <iamjoeanselm@gmail.com>
2022-04-04 17:17:27 +0100

    first commit

--- components/menu.js
+++ components/menu.js
@@ -6,27 +6,27 @@ import { SocialIcon } from "react-social-icons";
 export default function Menubar() {
   return (
     <div>
-      <nav class="relative z-10 rounded border-gray-200  px-2 py-2.5 dark:bg-gray-800 sm:px-4">
-        <div class="container mx-auto flex flex-wrap items-center justify-between">
-          <a href="/" class="flex items-center">
+      <nav className="relative z-10 rounded border-gray-200  px-2 py-2.5 dark:bg-gray-800 sm:px-4">
+        <div className="container mx-auto flex flex-wrap items-center justify-between">
+          <a href="/" className="flex items-center">
             <img
               src="img/oktlogo.png"
-              class="mr-3 h-6 sm:h-9"
+              className="mr-3 h-6 sm:h-9"
               alt="Flowbite Logo"
             />
-            <span class="self-center whitespace-nowrap text-xl font-semibold text-white">
+            <span className="self-center whitespace-nowrap text-xl font-semibold text-white">
               Oktosix
             </span>
           </a>
-          <div class="flex items-center md:order-2">
+          <div className="flex items-center md:order-2">
             <button
               type="button"
-              class="mr-3 flex rounded-full bg-gray-800 text-sm focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600 md:mr-0"
+              className="mr-3 flex rounded-full bg-gray-800 text-sm focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600 md:mr-0"
               id="user-menu-button"
               aria-expanded="false"
               data-dropdown-toggle="dropdown"
             >
-              <span class="sr-only">Open user menu</span>
+              <span className="sr-only">Open user menu</span>
               <SocialIcon
                 url="https://twitter.com/crisvytv"
                 style={{ height: 20, width: 20, marginRight: 10 }}
@@ -59,19 +59,19 @@ export default function Menubar() {
               data-popper-escaped=""
               data-popper-placement="top"
             >
-              <div class="py-3 px-4">
-                <span class="block text-sm text-gray-900 dark:text-white">
+              <div className="py-3 px-4">
+                <span className="block text-sm text-gray-900 dark:text-white">
                   Bonnie Green
                 </span>
-                <span class="block truncate text-sm font-medium text-gray-500 dark:text-gray-400">
+                <span className="block truncate text-sm font-medium text-gray-500 dark:text-gray-400">
                   name@flowbite.com
                 </span>
               </div>
-              <ul class="py-1" aria-labelledby="dropdown">
+              <ul className="py-1" aria-labelledby="dropdown">
                 <li>
                   <a
                     href="#"
-                    class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
+                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
                   >
                     Dashboard
                   </a>
@@ -79,7 +79,7 @@ export default function Menubar() {
                 <li>
                   <a
                     href="#"
-                    class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
+                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
                   >
                     Settings
                   </a>
@@ -87,7 +87,7 @@ export default function Menubar() {
                 <li>
                   <a
                     href="#"
-                    class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
+                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
                   >
                     Earnings
                   </a>
@@ -95,7 +95,7 @@ export default function Menubar() {
                 <li>
                   <a
                     href="#"
-                    class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
+                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
                   >
                     Sign out
                   </a>
@@ -105,13 +105,13 @@ export default function Menubar() {
             <button
               data-collapse-toggle="mobile-menu-2"
               type="button"
-              class="ml-1 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+              className="ml-1 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
               aria-controls="mobile-menu-2"
               aria-expanded="false"
             >
-              <span class="sr-only">Open main menu</span>
+              <span className="sr-only">Open main menu</span>
               <svg
-                class="h-6 w-6"
+                className="h-6 w-6"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -123,7 +123,7 @@ export default function Menubar() {
                 ></path>
               </svg>
               <svg
-                class="hidden h-6 w-6"
+                className="hidden h-6 w-6"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -137,14 +137,14 @@ export default function Menubar() {
             </button>
           </div>
           <div
-            class="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
+            className="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
             id="mobile-menu-2"
           >
-            <ul class="mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium">
+            <ul className="mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium">
               <li>
                 <a
                   href="#"
-                  class="block rounded bg-blue-700 py-2 pr-4 pl-3 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 lg:text-white"
+                  className="block rounded bg-blue-700 py-2 pr-4 pl-3 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 lg:text-white"
                   aria-current="page"
                 >
                   Home
@@ -153,7 +153,7 @@ export default function Menubar() {
               <li>
                 <a
                   href="#"
-                  class="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                  className="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
                 >
                   About
                 </a>
@@ -161,7 +161,7 @@ export default function Menubar() {
               <li>
                 <a
                   href="#"
-                  class="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                  className="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
                 >
                   Services
                 </a>
@@ -169,7 +169,7 @@ export default function Menubar() {
               <li>
                 <a
                   href="#"
-                  class="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                  className="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
                 >
                   Pricing
                 </a>
@@ -177,7 +177,7 @@ export default function Menubar() {
               <li>
                 <a
                   href="#"
-                  class="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
+                  className="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
                 >
                   Contact
                 </a>
