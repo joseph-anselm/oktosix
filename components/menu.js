a8270ff4953ff3ffffc4f5ea7584c2df228a975c
Joseph Anselm <iamjoeanselm@gmail.com>
2022-04-05 05:15:33 +0100

    menu fix

--- components/menu.js
+++ components/menu.js
@@ -95,28 +95,39 @@ export default function Menubar() {
           >
             <ul className="mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium">
               <Link href="/" className="">
-                Home
+                <span className="text-white hover:text-yellow-500 cursor-pointer">
+                  Home
+                </span>
               </Link>
               <Link href="/" className="text-white">
-                About
+                <span className="text-white hover:text-yellow-500 cursor-pointer">
+                  {" "}
+                  About
+                </span>
               </Link>
               <Link
                 href="/"
                 className="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
               >
-                Services
+                <span className="text-white hover:text-yellow-500 cursor-pointer">
+                  Services
+                </span>
               </Link>
               <Link
                 href="/about"
                 className="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
               >
-                Blogs
+                <span className="text-white hover:text-yellow-500 cursor-pointer">
+                  Blogs
+                </span>
               </Link>
               <Link
                 href="/contact"
                 className="block border-b border-gray-100 py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white"
               >
-                Contact
+                <span className="text-white hover:text-yellow-500 cursor-pointer">
+                  Contact
+                </span>
               </Link>
             </ul>
           </div>
