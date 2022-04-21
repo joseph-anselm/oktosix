5cdf317194c8a28e3b0c71456888a79d79fa5738
Joseph Anselm <iamjoeanselm@gmail.com>
2022-04-21 13:39:17 +0100

    homepage fix

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,144 @@
-import React from 'react'
+import React from "react";
 
 export default function contact() {
   return (
-    <div>contact</div>
-  )
+    <div>
+      <div class="relative w-full">
+        <header>
+          <nav class="fixed z-10 w-full border-b bg-white md:absolute md:bg-transparent">
+            <div class="container m-auto px-2 md:px-12 lg:px-7">
+              <div class="flex flex-wrap items-center justify-between gap-6 py-4 md:gap-0 md:py-4">
+                <div class="flex w-full justify-between px-6 md:px-0 lg:w-max">
+                  <a
+                    href="#"
+                    aria-label="logo"
+                    class="flex items-center space-x-2"
+                  >
+                    <div aria-hidden="true" class="flex space-x-1">
+                      <div class="h-4 w-4 rounded-full bg-gray-900"></div>
+                      <div class="h-6 w-2 bg-yellow-500"></div>
+                    </div>
+                    <span class="text-2xl font-bold text-gray-900">
+                      Arceelus
+                    </span>
+                  </a>
+
+                  <button
+                    aria-label="humburger"
+                    id="hamburger"
+                    class="relative -mr-2 h-10 w-10 lg:hidden"
+                  >
+                    <div
+                      aria-hidden="true"
+                      id="line"
+                      class="transtion inset-0 m-auto h-0.5 w-6 rounded bg-yellow-900 duration-300"
+                    ></div>
+                    <div
+                      aria-hidden="true"
+                      id="line2"
+                      class="transtion inset-0 m-auto mt-2 h-0.5 w-6 rounded bg-yellow-900 duration-300"
+                    ></div>
+                  </button>
+                </div>
+
+                <div class="hidden w-full flex-wrap items-center justify-end space-y-6 rounded-xl bg-white p-6 md:flex-nowrap md:space-y-0 md:bg-transparent md:p-0 lg:flex lg:w-7/12">
+                  <div class="text-gray-600 lg:pr-4">
+                    <ul class="space-y-6 text-sm font-medium tracking-wide md:flex md:space-y-0">
+                      <li>
+                        <a
+                          href="#"
+                          class="block transition hover:text-yellow-700 md:px-4"
+                        >
+                          <span>Home</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          class="block transition hover:text-yellow-700 md:px-4"
+                        >
+                          <span>Portfolio</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="#"
+                          class="block transition hover:text-yellow-700 md:px-4"
+                        >
+                          <span>Services</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+
+                  <div class="w-full space-y-2 border-yellow-200 md:w-max lg:space-y-0 lg:border-l">
+                    <button
+                      type="button"
+                      title="Start buying"
+                      class="w-full rounded-full py-3 px-6 text-center transition focus:bg-yellow-100 active:bg-yellow-200 sm:w-max"
+                    >
+                      <span class="block text-sm font-semibold text-yellow-800">
+                        Sign up
+                      </span>
+                    </button>
+                    <button
+                      type="button"
+                      title="Start buying"
+                      class="w-full rounded-full bg-yellow-300 py-3 px-6 text-center transition hover:bg-yellow-100 focus:bg-yellow-300 active:bg-yellow-400 sm:w-max"
+                    >
+                      <span class="block text-sm font-semibold text-yellow-900">
+                        Contact Us
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <div>
+          <img
+            class="absolute inset-0 top-[4.68rem] h-[90vh] w-full object-cover object-top"
+            src="images/bg2.webp"
+            alt="image"
+            loading="lazy"
+          />
+          <div class="container relative m-auto border-l border-r px-6 md:px-12 lg:px-7">
+            <div class="ml-auto py-40 md:w-9/12 lg:w-7/12 lg:py-56">
+              <h1 class="text-4xl font-bold text-gray-900 md:text-6xl lg:text-4xl xl:text-6xl">
+                Shaping a world with an endless{" "}
+                <span class="text-yellow-600">imagination.</span>
+              </h1>
+              <p class="mt-8 text-gray-700">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+                incidunt nam itaque sed eius modi error totam sit illum.
+                Voluptas doloribus asperiores quaerat aperiam. Quidem harum
+                omnis beatae ipsum soluta!
+              </p>
+              <div class="mt-16 space-y-2 sm:space-x-6 md:w-max lg:space-y-0">
+                <button
+                  type="button"
+                  title="Start buying"
+                  class="w-full rounded-full border border-gray-200 py-3 px-6 text-center transition focus:bg-yellow-100 active:bg-yellow-200 sm:w-max"
+                >
+                  <span class="block text-sm font-semibold text-yellow-800">
+                    About us
+                  </span>
+                </button>
+                <button
+                  type="button"
+                  title="Start buying"
+                  class="w-full rounded-full bg-yellow-300 py-3 px-6 text-center transition hover:bg-yellow-100 focus:bg-yellow-300 active:bg-yellow-400 sm:w-max"
+                >
+                  <span class="block text-sm font-semibold text-yellow-900">
+                    Contact Us
+                  </span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ import { SocialIcon } from "react-social-icons";
 export default function index() {
   return (
     <div className="wrapper">
-      <div className="header  absolute top-0  left-0 right-0 z-0  h-full w-full bg-gradient-to-tl from-purple-900 to-green-700">
+      <div className="header  absolute top-0 grid content-center  left-0 right-0 z-0  h-full w-full bg-gradient-to-tl from-purple-900 to-green-700">
         <img
           src="img/okto1.jpg"
           className=" absolute h-full w-full object-cover mix-blend-overlay"
         />
 
-        <div className=" relative top-60  text-white">
+        <div className="relative grid content-center  text-white">
           <div className="ml-5 ">
             <h1 className=" mb-5 text-6xl">Oktosix Motions Pictures</h1>
             <p>Unlimited Entertainment, Movies & More!</p>
@@ -31,6 +31,79 @@ export default function index() {
               </svg>{" "}
               Watch Now!
             </button>
+          </div>
+        </div>
+      </div>
+      <button
+        className="block rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="button"
+        data-modal-toggle="default-modal"
+      >
+        Toggle modal
+      </button>
+
+      <div
+        id="default-modal"
+        aria-hidden="true"
+        className="h-modal fixed right-0 left-0 top-4 z-50 hidden items-center justify-center overflow-y-auto overflow-x-hidden md:inset-0 md:h-full"
+      >
+        <div className="relative h-full w-full max-w-2xl px-4 md:h-auto">
+          <div className="relative rounded-lg bg-white shadow dark:bg-gray-700">
+            <div className="flex items-start justify-between rounded-t border-b p-5 dark:border-gray-600">
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-white lg:text-2xl">
+                Terms of Service
+              </h3>
+              <button
+                type="button"
+                className="ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
+                data-modal-toggle="default-modal"
+              >
+                <svg
+                  className="h-5 w-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+
+            <div className="space-y-6 p-6">
+              <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                With less than a month to go before the European Union enacts
+                new consumer privacy laws for its citizens, companies around the
+                world are updating their terms of service agreements to comply.
+              </p>
+              <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                The European Union’s General Data Protection Regulation
+                (G.D.P.R.) goes into effect on May 25 and is meant to ensure a
+                common set of data rights in the European Union. It requires
+                organizations to notify users as soon as possible of high-risk
+                data breaches that could personally affect them.
+              </p>
+            </div>
+
+            <div className="flex items-center space-x-2 rounded-b border-t border-gray-200 p-6 dark:border-gray-600">
+              <button
+                data-modal-toggle="default-modal"
+                type="button"
+                className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                I accept
+              </button>
+              <button
+                data-modal-toggle="default-modal"
+                type="button"
+                className="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-900 focus:z-10 focus:ring-4 focus:ring-gray-300 dark:border-gray-500 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+              >
+                Decline
+              </button>
+            </div>
           </div>
         </div>
       </div>
